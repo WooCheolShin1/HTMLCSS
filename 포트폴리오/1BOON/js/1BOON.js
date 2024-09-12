5be1afd67318659717e39c3f2ff13f26570ea14b
WooCheolShin <sigon1112@naver.com>
2024-09-12 17:44:06 +0900

    first commit

--- 포트폴리오/1BOON/js/1BOON.js
+++ 포트폴리오/1BOON/js/1BOON.js
@@ -12,7 +12,19 @@
       }
     });
     //=========================================================================//
-    
+
+    //===============================로그인 부분================================//
+    $("input[id='autologin']").change(function(){
+      var text = $("input[id='autologin']:checked").val();
+      if(confirm("자동로그인을 사용하시면 다음부터 회원아이디와 비밀번호를 입력하실 필요가 없습니다.\n\n공공장소에서는 개인정보가 유출될수 있으니 사용을 자제하여 주십시오.\n\n자동로그인을 사용하시겠습니까?") == true){
+        $("#autologin").prop("checked", true);
+      }
+      else{
+        $("#autologin").prop("checked", false);
+      }
+    })
+    //========================================================================//
+
     let btn = document.querySelectorAll(".wrap .button li");
     let ilboonmenu = document.querySelectorAll(".wrap .wrapimage .swiper .swiper-wrapper");
     let sil = document.querySelector(".wrap .wrapimage");
